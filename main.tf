f4bdcf0ce61602a24d2f3f22d192f9d4bff3cc05
iBrid <col.buche02@gmail.com>
2025-10-11 14:31:19 -0600

    no changes

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_version = ">=1.3.0"
   required_providers {
-    azurerm ={
-        source = "hashicorp/azurerm"
-        version = ">=4.0.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=4.0.0"
     }
   }
-  cloud { 
-    organization = "DatacentR" 
-    workspaces { 
-      name = "az-infrastructure-creation-w-tf" 
-    } 
-  } 
+  cloud {
+    organization = "DatacentR"
+    workspaces {
+      name = "az-infrastructure-creation-w-tf"
+    }
+  }
 }
 
 provider "azurerm" {
@@ -20,16 +20,16 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "myTerraformRG"
+  name     = "myTerraformRG"
   location = "West US 2"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name = "mytfstg02211"
-  resource_group_name = azurerm_resource_group.rg.name
-  location = "West US 2"
+  name                     = "mytfstg02211"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = "West US 2"
   account_replication_type = "LRS"
-  account_tier = "Standard"
+  account_tier             = "Standard"
 
   tags = {
     environment = "test env"
