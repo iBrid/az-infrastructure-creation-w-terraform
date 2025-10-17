ddfb8969287f371076e34f2609c1e495e7469f1f
iBrid <col.buche02@gmail.com>
2025-10-17 15:11:20 -0600

    Updating NS rule

--- main.tf
+++ main.tf
@@ -68,6 +68,6 @@ resource "azurerm_network_security_rule" "nsrule" {
   priority                    = 100
   source_port_range           = "*"
   destination_port_range      = "3389"
-  source_address_prefix = "*"
-  destination_address_prefix = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
 }
